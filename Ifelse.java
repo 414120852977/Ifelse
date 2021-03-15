744c12abc89ec3e38a3c5d5446fce845889b8d7a
Ashok Mane <ashokmane81800@gmail.com>
2021-03-15 16:34:44 +0530

    [ashok] created a file to converting di

---
+++ Ifelse.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Ifelse{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enetr the number between 1 to 9 to display single digit number :");
+int x=sc.nextInt();
+if(x==1){
+System.out.println("one");
+}
+else if(x==2){
+System.out.println("two");
+}
+else if(x==3){
+System.out.println("three");
+}
+else if(x==4){
+System.out.println("four");
+}
+else if(x==5){
+System.out.println("five");
+}
+else if(x==6){
+System.out.println("six");
+}
+else if(x==7){
+System.out.println("seven");
+}
+else if(x==8){
+System.out.println("eight");
+}
+else{
+System.out.println("nine");
+}
+
+}
+}
+
+
