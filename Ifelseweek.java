9c32296964f13df90871e7a2235e4d8c492f4ba0
Ashok Mane <ashokmane81800@gmail.com>
2021-03-15 16:41:24 +0530

    [ashok] created a file to diplay week

---
+++ Ifelseweek.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Ifelse{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enetr the number between 1 to 7 to display day :");
+int x=sc.nextInt();
+if(x==1){
+System.out.println("sunday");
+}
+else if(x==2){
+System.out.println("monday");
+}
+else if(x==3){
+System.out.println("tuesday");
+}
+else if(x==4){
+System.out.println("wenesday");
+}
+else if(x==5){
+System.out.println("thursday");
+}
+else if(x==6){
+System.out.println("friday");
+}
+else (x==7){
+System.out.println("saturday");
+}
+
+}
+}
